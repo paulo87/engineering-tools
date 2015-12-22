3009c2de63b26c5bd3656d842e6a1b95a550abe7
paulo87 <palex87@gmail.com>
2015-12-22 12:00:31 +0000

    make index.php redirect to the yii application

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
- 
-    phpinfo();
-    
+header('HTTP/1.1 307 Temporary Redirect');
+header('Location: engineering-tools/');
 ?>
+<html>
+  <head>
+    <title>Moved</title>
+  </head>
+  <body>
+    <h1>Moved</h1>
+    <p>This page has moved to <a href="engineering-tools/">engineering-tools</a>.</p>
+  </body>
+</html>
